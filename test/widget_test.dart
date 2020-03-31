28e192ae908066a51015b369afc9663dcd560b6a
Anmol Sahoo <anmol.sahoo25@gmail.com>
2020-03-31 19:35:13 +0530

    Added the dashboard page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hello1/main.dart';
+import 'package:covid19_dtrack/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
